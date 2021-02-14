ffc835b7903dd09a70dcc4eb4460c4d8d5553890
dkyryk <kyryk.dmytro@gmail.com>
2021-02-14 18:10:44 +0200

    coroutines

--- src/main/kotlin/com/ercarts/simple/Coroutines.kt
+++ src/main/kotlin/com/ercarts/simple/Coroutines.kt
@@ -11,18 +11,54 @@ fun main() {
 
     measureTime(name = "sync", call = ::syncCall)
     measureTime(name = "async", call = ::asyncCall)
+    measureTime(name = "launch", call = ::launchCall)
+    backgroundLaunch()
+
+    runBlocking {
+        println("Waiting till background finished")
+        delay(3000)
+    }
+}
+fun backgroundLaunch() {
+    val count = 5
+    val name = "launchBackground"
+
+    GlobalScope.launch {
+        wrapCall(name) {
+            runBlocking {
+                val time = measureTimeMillis {
+                    (1..count).map {
+                        async {
+                            randomDelay()
+                        }
+                    }.awaitAll()
+                }
+                timeTaken(count, name, time)
+            }
+        }
+    }
+
+}
+
+fun timeTaken(count: Int, name: String, time: Long) {
+    println("$count calls to $name took: $time milliseconds")
 }
 
 fun measureTime(name: String, count: Int = 5, call: (Int) -> Unit) {
-    println("-------------------------------------------------------------")
-    println("$name execution")
-    val time = measureTimeMillis {
-        call(count)
+    wrapCall(name = name) {
+        val time = measureTimeMillis {
+            call(count)
+        }
+        timeTaken(count, name, time)
     }
-    println("$count calls to $name took: $time milliseconds")
-    println("-------------------------------------------------------------")
 }
 
+fun wrapCall(name: String, call: () -> Unit) {
+    println("-------------------------------------------------------------")
+    println("$name execution")
+    call()
+    println("-------------------------------------------------------------")
+}
 
 fun asyncCall(count: Int) = runBlocking {
     (1..count).map {
@@ -38,8 +74,17 @@ fun syncCall(count: Int) = runBlocking {
     }
 }
 
+fun launchCall(count: Int) = runBlocking {
+    (1..count).map {
+        launch(Dispatchers.Default) {
+            randomDelay()
+        }
+    }
+}
+
 suspend fun randomDelay() {
+
     val delayMs = Random.nextLong(500, 1500)
     delay(delayMs)
-    println("Waited $delayMs milliseconds")
+    println("Executed in ${Thread.currentThread().name} and waited $delayMs milliseconds")
 }
