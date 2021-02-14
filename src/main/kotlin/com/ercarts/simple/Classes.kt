eaae11a040f3b7d9f64edbc1b4088bde0bbb67a7
dkyryk <kyryk.dmytro@gmail.com>
2021-02-14 17:06:22 +0200

    classes

--- src/main/kotlin/com/ercarts/simple/Classes.kt
+++ src/main/kotlin/com/ercarts/simple/Classes.kt
@@ -5,7 +5,9 @@ package com.ercarts.simple
  */
 
 interface Point {
-    fun coordinates(): String
+    fun coordinates(): String {
+        return "Default logic! coordinates unknown"
+    }
 }
 
 class PointA internal constructor(x: Int, y: Int) : Point {
@@ -36,6 +38,15 @@ data class PointC(private val x: Int, private val y: Int) : Point {
     override fun coordinates(): String = "dx=$x|dy=$y"
 }
 
+class PointD private constructor(private val x: Int, private val y: Int) : Point {
+
+    internal companion object Factory {
+        fun newInstance(x: Int, y: Int): Point {
+            return PointD(x, y)
+        }
+    }
+}
+
 fun main() {
     val a = PointA(1, 0)
     val a1 = PointA(1)
@@ -43,8 +54,9 @@ fun main() {
     val c = PointC(6, 7)
     val cCopy = c.copy(x = 8)
     val cEqualsCopy = c.copy()
+    val d = PointD.newInstance(8,234)
 
-    listOf(a, a1, b, c, cCopy, cEqualsCopy)
+    listOf(a, a1, b, c, cCopy, cEqualsCopy, d)
         .forEach {
             println("HC = ${it.hashCode()} | toS = ${it.toString()} | coordinates = ${it.coordinates()}")
         }
